995c15710d0e65e6d02f8c716aa58e38bf5e4ba3
yoni2k <yoni.k81@gmail.com>
2019-08-28 13:34:25 +0300

    Better version with some code improvements - building the processed data as we process each column

--- preprocessing.py
+++ preprocessing.py
@@ -4,9 +4,6 @@ import pandas as pd
 Goal: predict employee absence from work based on numerous factors.  
 See input file "inputs/Absenteeism-data.csv" for different inputs and factors.
 Also, see PREPROCESSING.md for analysis of inputs and decisions for preprocessing
-
-TODO:
-- Update requirements.txt
 """
 
 INPUT_FILE = "inputs/Absenteeism-data.csv"
@@ -29,17 +26,16 @@ def preprocess():
     print(f'Number 0s in Absenteeism Time in Hours: {df[df["Absenteeism Time in Hours"]==0].shape[0]}')
 
     # ------------- ID
-    # Dropping ID variable. Reason: we would like to predict not based on who the person is, but his characteristics
-    df = df.drop(['ID'], axis=1)
+    # Not taking ID column. Reason: we would like to predict not based on who the person is, but his characteristics
 
     # ------------- Reason for Absence
-    # Changing reason into a cetegorical value and categorize together. It's OK to drop first since it has 38/700 > 5% observations
-    print('Reason for Absence - adding dummy variables - number of values of each category before dropping first value:')
-    print(pd.get_dummies(df['Reason for Absence']).sum(axis=0))
-    # TODO - understand why we are dropping first, and then categorizing, should have perhaps first categories, then dropped?
+    # Changing reason into a cetegorical value and categorize together.
+    #   It's OK to drop first since it has 38/700 > 5% observations
+    print('Reasons for Absence - adding dummy variables - number of values of each category before dropping first value:')
+    print(df['Reason for Absence'].value_counts())
+    # TODO - understand why we are dropping first, and then categorizing,
+    #  should have perhaps first categories, then dropped?
     reason_columns = pd.get_dummies(df['Reason for Absence'], drop_first=True)
-    # reason_columns = pd.get_dummies(df['Reason for Absence'], prefix='R')
-    df = df.drop(['Reason for Absence'], axis=1)
     print('============ Head of reasons dummy variables')
     print(reason_columns.head().to_string())
 
@@ -49,73 +45,63 @@ def preprocess():
     reason_type_2 = reason_columns.loc[:, 15:17].sum(axis=1)
     reason_type_3 = reason_columns.loc[:, 18:21].sum(axis=1)
     reason_type_4 = reason_columns.loc[:, 22:].sum(axis=1)
-    print("reason_type_1 head:\n" + reason_type_1.head().to_string())
-    print("reason_type_2 head:\n" + reason_type_2.head().to_string())
-    print("reason_type_3 head:\n" + reason_type_3.head().to_string())
-    print("reason_type_4 head:\n" + reason_type_4.head().to_string())
+    print(f'Sum of all reasons: {sum(reason_type_1) + sum(reason_type_2) + sum(reason_type_3) + sum(reason_type_4)}')
 
-    df_reasons = pd.concat([reason_type_1, reason_type_2, reason_type_3, reason_type_4], axis=1)
-    df_reasons.columns = ['Reason_1', 'Reason_2', 'Reason_3', 'Reason_4']
-    df = pd.concat([df_reasons, df], axis=1)
-    print('============ Head of df after adding reasons')
-    print(df.head().to_string())
-    print(f'Sum of all reasons: {sum(df["Reason_1"]) + sum(df["Reason_2"]) + sum(df["Reason_3"]) + sum(df["Reason_4"])}')
+    df_preprocessed = pd.concat([reason_type_1, reason_type_2, reason_type_3, reason_type_4], axis=1)
+    df_preprocessed.columns = ['Reason_1', 'Reason_2', 'Reason_3', 'Reason_4']
+    print('============ Head of df_preprocessed after adding reasons')
+    print(df_preprocessed.head().to_string())
 
     # ------------- Date
     # Remove only month and day of week values, since we presume that's what might affect the absenteeism
-    # TODO - what are we doing with dates - this format is probably better, but still not the best?
+    # Change to datetime64 format
     print(f"Type of date field: {type(df['Date'][0])}")
     df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
-    print(f'================ After performing date converstion, shape: {df.shape}, Head: ')
+    print(f'================ After performing date conversion, shape: {df.shape}, Head: ')
     print(df.head().to_string())
     print(f"Date - min: {min(df['Date'])}, max: {max(df['Date'])}, unique: {df['Date'].unique().shape}, null: {df['Date'].isnull().sum()}")
 
-    df['Month Value'] = pd.DatetimeIndex(df['Date']).month
-    df['Day of the Week'] = pd.DatetimeIndex(df['Date']).dayofweek
-    df = df.drop(['Date'], axis=1)
-    # reorder columns
-    df = df[['Reason_1', 'Reason_2', 'Reason_3', 'Reason_4', 'Month Value', 'Day of the Week',
-             'Transportation Expense', 'Distance to Work', 'Age', 'Daily Work Load Average', 'Body Mass Index',
-             'Education', 'Children', 'Pets', 'Absenteeism Time in Hours']]
-    print(f'================ After extracting relevant date fields, shape: {df.shape}, Head: ')
-    print(df.head().to_string())
+    df_preprocessed['Month Value'] = pd.DatetimeIndex(df['Date']).month
+    df_preprocessed['Day of the Week'] = pd.DatetimeIndex(df['Date']).dayofweek
+    print(f'================ After adding month and day of week fields, shape: {df_preprocessed.shape}, Head: ')
+    print(df_preprocessed.head().to_string())
+
+    # Take the following as is:
+    #  'Transportation Expense','Distance to Work', 'Age', 'Daily Work Load Average', 'Body Mass Index'
+    df_preprocessed['Transportation Expense'] = df['Transportation Expense']
+    df_preprocessed['Distance to Work'] = df['Distance to Work']
+    df_preprocessed['Age'] = df['Age']
+    df_preprocessed['Daily Work Load Average'] = df['Daily Work Load Average']
+    df_preprocessed['Body Mass Index'] = df['Body Mass Index']
+    print(f"================ After adding 'Transportation Expense','Distance to Work', 'Age', 'Daily Work Load Average',"
+          f" 'Body Mass Index', shape: {df_preprocessed.shape}, Head: ")
+    print(df_preprocessed.head().to_string())
 
     # ------------- Education
     # Since there are very few non-1's, combine the rest together
+    # TODO - consider having 3 categories (combining just 3 & 4 and not 1,2,3 together),
+    #   since 4th category is the only one that's really small
+    #   Then according to the course, need to add dummy values - turn from categorical
     print(f'frequency of educations:\n{df["Education"].value_counts()}')
-    df['Education'] = df['Education'].map({1:0, 2:1, 3:1, 4:1})
-    print(f'frequency of educations after turning into 0 and 1:\n{df["Education"].value_counts()}')
-    print(f'================ After turning education into dummies, shape: {df.shape}, Head: ')
-    print(df.head().to_string())
+    df_preprocessed['Education'] = df['Education'].map({1: 0, 2: 1, 3: 1, 4: 1})
+    print(f'frequency of educations after turning into 0 and 1:\n{df_preprocessed["Education"].value_counts()}')
+    print(f'================ After turning education into dummies, shape: {df_preprocessed.shape}, Head: ')
+    print(df_preprocessed.head().to_string())
 
-    # TODO - consider having 3 categories (combining just 3 & 4 as below)
-    """
-    education_columns = pd.get_dummies(df["Education"], drop_first=True)
-    education_columns.columns = ['Ed_2', 'Ed_3', 'Ed_4']
-    print(f'================ Education dummies, shape: {education_columns.shape}, Head: ')
-    print(education_columns.head(10).to_string())
-    education_columns['Ed_3_4'] = education_columns['Ed_3'] + education_columns['Ed_4']
-    education_columns = education_columns.drop(['Ed_3', 'Ed_4'], axis=1)
-    print(f'================ Education dummies after combining Education 3 and 4, shape: {education_columns.shape}, Head: ')
-    print(education_columns.head(10).to_string())
-
-    df = df.drop(['Education'], axis=1)
-    df = pd.concat([df, education_columns], axis=1)
-    df = df[['Reason_1', 'Reason_2', 'Reason_3', 'Reason_4', 'Month Value', 'Day of the Week', 'Transportation Expense',
-             'Distance to Work', 'Age', 'Daily Work Load Average', 'Body Mass Index', 'Ed_2', 'Ed_3_4',
-             'Children', 'Pets', 'Absenteeism Time in Hours']]
-    print(df.columns.values)
-    print(f'================ After turning education into dummies, shape: {df.shape}, Head: ')
-    print(df.head().to_string())
-    """
+    # Adding as is 'Children', 'Pets', 'Absenteeism Time in Hours'
+    df_preprocessed['Children'] = df['Children']
+    df_preprocessed['Pets'] = df['Pets']
+    df_preprocessed['Absenteeism Time in Hours'] = df['Absenteeism Time in Hours']
+    print(f"================ After adding 'Children', 'Pets', 'Absenteeism Time in Hours' - final version, "
+          f"shape: {df_preprocessed.shape}, Head: ")
+    print(df_preprocessed.head().to_string())
 
     # For some reason, even though the input round column 'Daily Work Load Average' rounded to 3 places, in output
     #  some values are with many more places, so rounded it
-    df['Daily Work Load Average'] = round(df['Daily Work Load Average'], 3)
+    df_preprocessed['Daily Work Load Average'] = round(df_preprocessed['Daily Work Load Average'], 3)
 
-    print(f'================ After resetting index, shape: {df.shape}, Head: ')
-    print(df.head().to_string())
-    df.to_csv(OUTPUT_FILE, index=False)
+    # Write out to a csv file for future use
+    df_preprocessed.to_csv(OUTPUT_FILE, index=False)
 
 
 preprocess()
